c4ecc3033ff3f518c277e8a06cb248421f3061b5
Ning <nche0024@gmail.com>
2021-02-03 15:24:25 +1100

    binding html

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div>{{great}}, {{name}}</div>
+  <div>{{channel}} - new</div>
+  <div v-html='channel'></div>
   <div v-text='channel'></div>
+  <div v-html='hack'></div>
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
   data() {
     return{
       great: "hello",
       name: 'ning',
-      channel: 'rarely used'
+      channel: '<b>rarely used</b>',
+      hack:`<a href='#' onclick='alert("You have been hacked!")'>Win a Price!</a>`
     }
   },
 }

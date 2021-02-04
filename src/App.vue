a9da85c5eea57037563da6100db7fedea70c4a2a
Ning <nche0024@gmail.com>
2021-02-04 16:15:07 +1100

    form handling v-model

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,89 @@
 <template>
-  <h2>{{name}}</h2>
-  <button v-on:click="changeName(), increment()">Change Name</button>
+  <p>{{formValues.yearsOfExperience}}</p>
+  <form @submit="submitForm">
+    <div>
+      <label for='name'>Name</label>
+      <input type='text' v-model='formValues.name'/>
+    </div>
 
-  <h2>{{count}}</h2>
-  <button v-on:click="increment">Add</button>
-  <button @click="count = count - 1">Minus</button>
-  <div>
-    <input value=/>
-  </div>
+    <!-- text field -->
+    <div>
+      <label for='profile'>Profile</label>
+      <input type='text' v-model='formValues.profileSummary'/>
+    </div>
+
+    <!-- single selection -->
+    <div>
+      <label for='country'>Country</label>
+      <select id='country' v-model="formValues.country">
+        <option value=''>Select a country</option>
+        <option value='india'>India</option>
+        <option value='vietnam'>Vietnam</option>
+        <option value='singapore'>singapore</option>
+      </select>
+    </div>
+
+    <!-- multiple selection -->
+    <div>
+      <label for='location'>Location</label>
+      <select id='location' multiple v-model="formValues.jobLocation">
+        <option value='india'>India</option>
+        <option value='vietnam'>Vietnam</option>
+        <option value='singapore'>singapore</option>
+      </select>
+    </div>
+
+    <!-- single checkbox -->
+    <div>
+      <input type='checkbox' id='remoteWork' v-model="formValues.remoteWork" true-value='yes' false-value='no'/>
+      <label for='remoteWork'>Open to remote work</label>
+    </div>
+
+    <!-- multiple checkbox -->
+    <div>
+      <label>Skill Set</label>
+      <input type='checkbox' id='html' value='html' v-model='formValues.skillSet'/>
+      <label for='html'>HTML</label>
+      <input type='checkbox' id='css' value='css' v-model='formValues.skillSet'/>
+      <label for='css'>CSS</label>
+      <input type='checkbox' id='JS' value='JS' v-model='formValues.skillSet'/>
+      <label for='js'>JS</label>
+    </div>
+
+    <!-- radio button -->
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type='radio'
+        id='0-2'
+        value='0-2'
+        v-model='formValues.yearsOfExperience'
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type='radio'
+        id='3-5'
+        value='3-5'
+        v-model='formValues.yearsOfExperience'
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type='radio'
+        id='6-10'
+        value='6-10'
+        v-model='formValues.yearsOfExperience'
+      />
+      <label for="6-10">6-10</label>
+    </div>
+
+    <!-- submit button -->
+    <div>
+      <button>Submit</button>
+    </div>
+
+
+  </form>
+  <div></div>
 </template>
 
  
@@ -16,20 +92,21 @@ export default {
   name: 'App',
   data() {
     return{
-      name:'ning',
-      count: 0,
-      inputValue: 0
+      formValues: {
+        name: '',
+        profileSummary:'',
+        country: '',
+        jobLocation: [],
+        remoteWork: 'no',
+        skillSet: [],
+        yearsOfExperience: ''
+      }
     }
   }, 
   methods: {
-    increment(){
-      this.count += 1
-    },
-    decrement(){
-      this.count -= 1
-    },
-    changeName(){
-      this.name = 'batman'
+    submitForm(event){
+      event.preventDefault() //prevent refresh
+      console.log(this.formValues)
     }
   }
 }

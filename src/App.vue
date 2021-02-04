6d57c8c7ea2a4ef4c53b0a9ac2d720580ee8936a
Ning <nche0024@gmail.com>
2021-02-04 23:43:37 +1100

    computed property

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,10 @@
 <template>
-  <p v-once>{{formValues.name.length}}</p> 
-  <!-- v-once render static dom once -->
-
-  <p v-pre>{{formValues.name.length}}</p> 
-  <!-- v-pre not to compile html  -->
-
-  <!-- prevent modifier -->
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for='name'>Name</label>
-      <input type='text' v-model.trim.lazy='formValues.name'/>
-      <!-- <input @keyup.enter='submitForm' type='text'/> -->
-      <!-- submit after press enter -->
-    </div>
-
-    <!-- text field -->
-    <div>
-      <label for='profile'>Profile</label>
-      <input type='text' v-model='formValues.profileSummary'/>
-    </div>
-
-    <div>
-      <label for='age'>Age</label>
-      <input type='text' v-model.number='formValues.age'/>
-    </div>
-
-    <!-- single selection -->
-    <div>
-      <label for='country'>Country</label>
-      <select id='country' v-model="formValues.country">
-        <option value=''>Select a country</option>
-        <option value='india'>India</option>
-        <option value='vietnam'>Vietnam</option>
-        <option value='singapore'>singapore</option>
-      </select>
-    </div>
-
-    <!-- multiple selection -->
-    <div>
-      <label for='location'>Location</label>
-      <select id='location' multiple v-model="formValues.jobLocation">
-        <option value='india'>India</option>
-        <option value='vietnam'>Vietnam</option>
-        <option value='singapore'>singapore</option>
-      </select>
-    </div>
-
-    <!-- single checkbox -->
-    <div>
-      <input type='checkbox' id='remoteWork' v-model="formValues.remoteWork" true-value='yes' false-value='no'/>
-      <label for='remoteWork'>Open to remote work</label>
-    </div>
-
-    <!-- multiple checkbox -->
-    <div>
-      <label>Skill Set</label>
-      <input type='checkbox' id='html' value='html' v-model='formValues.skillSet'/>
-      <label for='html'>HTML</label>
-      <input type='checkbox' id='css' value='css' v-model='formValues.skillSet'/>
-      <label for='css'>CSS</label>
-      <input type='checkbox' id='JS' value='JS' v-model='formValues.skillSet'/>
-      <label for='js'>JS</label>
-    </div>
-
-    <!-- radio button -->
-    <div>
-      <label>Years of Experience</label>
-      <input
-        type='radio'
-        id='0-2'
-        value='0-2'
-        v-model='formValues.yearsOfExperience'
-      />
-      <label for="0-2">0-2</label>
-      <input
-        type='radio'
-        id='3-5'
-        value='3-5'
-        v-model='formValues.yearsOfExperience'
-      />
-      <label for="3-5">3-5</label>
-      <input
-        type='radio'
-        id='6-10'
-        value='6-10'
-        v-model='formValues.yearsOfExperience'
-      />
-      <label for="6-10">6-10</label>
-    </div>
-
-    <!-- submit button -->
-    <div>
-      <button @click="submitform">Submit</button>
-    </div>
-
-
-  </form>
-  <div></div>
+  <h2>fullname - {{firstName}} {{lastName}}</h2>
+  <h2>computed fullname - {{fullname}}</h2>
+  <div>
+    <h2>computed total - {{total}}</h2>
+    <button @click="addItem">Add</button>
+  </div>
 </template>
 
  
@@ -105,24 +13,41 @@ export default {
   name: 'App',
   data() {
     return{
-      formValues: {
-        name: '',
-        profileSummary:'',
-        country: '',
-        jobLocation: [],
-        remoteWork: 'no',
-        skillSet: [],
-        yearsOfExperience: '',
-        age: null
-      },
-      bonus:'bonus'
+      firstName:'Bruce',
+      lastName:'Wayue',
+      items:[
+        {
+          id: 1,
+          price: 200
+        },
+        {
+          id: 2,
+          price: 200
+        },
+        {
+          id: 3,
+          price: 600
+        }
+      ]
     }
-  }, 
-  methods: {
-    submitForm(event){
-      event.preventDefault() //prevent refresh
-      console.log(this.formValues)
+  },
+  methods:{
+    addItem(){
+      this.items.push({id: 4, price: 200})
+      console.log(this.items)
     }
+  },
+  computed:{
+    fullname(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    total(){
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    }
+
+    // computed properties are cashed
+    // once an independent property is changed
+    // the computed result will not be recalculated
   }
 }
 

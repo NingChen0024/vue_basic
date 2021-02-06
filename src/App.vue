728a48d0ff8cda446d31ea96f339a91471d25970
Ning <nche0024@gmail.com>
2021-02-06 15:09:45 +1100

    static and dynamic props

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,28 @@
 <template>
- 
-  <h2>volume tracker (0 - 20)</h2>
-  <h3>Current volume {{volume}}</h3>
-  <div>
-    <button @click='volume += 2'>Increase</button>
-    <button @click="volume -= 2">Decrease</button>
-  </div>
+  <Great name='ning' heroName='batman'/> 
+  <!-- static props -->
+  <Great :name='name'/>
+  <!-- dynamic props -->
 
-  <input type='text' v-model='movie'/>
-  <div>
-    <p>movie title</p>
-    <input type='text' v-model='movieInfo.title'>
-  </div>
-  <div>
-    <p>movie actor</p>
-    <input type='text' v-model='movieInfo.actor'/>
-  </div>
-
-  <button @click='movieList.push("bat4")'>push new movie</button>
 </template>
 
-
-
- 
 <script>
+import Great from './components/Great'
+// import sub component
+
 export default {
   name: 'App',
+  components: {
+    Great,
+  }, // informing component
   data() {
     return{
-      volume: 0,
-      movie: 'batman',
-      movieInfo: {
-        title: '',
-        actor:''
-      },
-      movieList: [
-        'bat',
-        'bat2',
-        'bat3'
-      ]
+      name: 'ningpp'
     }
   },
   methods: {},
   computed:{},
-  watch: {
-    volume(newValue, oldValue) {
-      if (oldValue < newValue && newValue === 16){
-        alert('volume is too high')
-      }
-    },
-    movie: {
-      handler(newValue){
-        console.log(`calling api=${newValue}`)
-      },
-      immediate: true
-      // handler will run after the page is loaded
-    },
-    movieInfo: {
-      handler(newValue){
-        console.log(`movie title: ${newValue.title}, movie actor: ${newValue.actor}`)
-      },
-      deep: true
-      // watch deep nested properties
-    },
-    movieList: {
-      handler(){
-        console.log(this.movieList)
-      },
-      deep: true
-    } 
-    // adding item doesn't change the reference of the array,so need deep
-    // watch array with deep
-  }
+  watch: {}
 }
 
 </script>
---
+++ src/components/Great.vue
@@ -0,0 +1,19 @@
+// passing props
+
+<template>
+  <div>
+    {{ name }} is {{heroName}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Great',
+    props:['name', 'heroName']
+  }
+</script>
+
+<style scoped>
+
+</style>
+
